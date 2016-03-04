2b1799b165ae8caabe6ecb5ddad716156d2ebffa
unknown <C5050685@DAL3DWCE96A-049.collaboration.ONEACS.COM>
2016-03-04 14:24:09 +0530

    Fully working version with setup and folder renamed as Angular Utilities

---
+++ AngularUtilities/frmAngularFactories.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmAngularFactories
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAngularFactories));
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnJsonSelect = new System.Windows.Forms.Button();
+            this.btnDestSelect = new System.Windows.Forms.Button();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.JsonFileSelector = new System.Windows.Forms.OpenFileDialog();
+            this.DestFolderSelector = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createAngularFactoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createJasmineTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create JSON Tree";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(540, 33);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(371, 361);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter the Source JSON Path";
+            // 
+            // txtSource
+            // 
+            this.errorProvider1.SetError(this.txtSource, "Please Select the JSON file");
+            this.txtSource.Location = new System.Drawing.Point(195, 35);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(185, 20);
+            this.txtSource.TabIndex = 3;
+            // 
+            // btnJsonSelect
+            // 
+            this.btnJsonSelect.Location = new System.Drawing.Point(386, 33);
+            this.btnJsonSelect.Name = "btnJsonSelect";
+            this.btnJsonSelect.Size = new System.Drawing.Size(36, 23);
+            this.btnJsonSelect.TabIndex = 4;
+            this.btnJsonSelect.Text = "...";
+            this.btnJsonSelect.UseVisualStyleBackColor = true;
+            this.btnJsonSelect.Click += new System.EventHandler(this.btnJsonSelect_Click);
+            // 
+            // btnDestSelect
+            // 
+            this.btnDestSelect.Location = new System.Drawing.Point(386, 72);
+            this.btnDestSelect.Name = "btnDestSelect";
+            this.btnDestSelect.Size = new System.Drawing.Size(36, 23);
+            this.btnDestSelect.TabIndex = 7;
+            this.btnDestSelect.Text = "...";
+            this.btnDestSelect.UseVisualStyleBackColor = true;
+            this.btnDestSelect.Click += new System.EventHandler(this.btnDestSelect_Click);
+            // 
+            // txtDest
+            // 
+            this.errorProvider1.SetError(this.txtDest, "Please select the Destination folder");
+            this.txtDest.Location = new System.Drawing.Point(195, 74);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(185, 20);
+            this.txtDest.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter the Destination Folder Path";
+            // 
+            // JsonFileSelector
+            // 
+            this.JsonFileSelector.Filter = "\"json file\" | *.json";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 383);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Create Angular Factories";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(28, 281);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(464, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(36, 228);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(0, 13);
+            this.lblProgress.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(972, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createAngularFactoriesToolStripMenuItem,
+            this.createJasmineTestToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // createAngularFactoriesToolStripMenuItem
+            // 
+            this.createAngularFactoriesToolStripMenuItem.Name = "createAngularFactoriesToolStripMenuItem";
+            this.createAngularFactoriesToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.createAngularFactoriesToolStripMenuItem.Text = "Create Angular Factories";
+            // 
+            // createJasmineTestToolStripMenuItem
+            // 
+            this.createJasmineTestToolStripMenuItem.Name = "createJasmineTestToolStripMenuItem";
+            this.createJasmineTestToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.createJasmineTestToolStripMenuItem.Text = "Create Jasmine Test ";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(353, 383);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(140, 23);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmAngularFactories
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 434);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnDestSelect);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnJsonSelect);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "frmAngularFactories";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Angular Factories";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnJsonSelect;
+        private System.Windows.Forms.Button btnDestSelect;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog JsonFileSelector;
+        private System.Windows.Forms.FolderBrowserDialog DestFolderSelector;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createAngularFactoriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createJasmineTestToolStripMenuItem;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ AngularUtilities/frmJasmine.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmJasmine : Form
+    {
+        DataTable dt =new DataTable();
+        Int32 intParentId = 0;
+        bool bEditMode = false;
+        Int32 intGridRow = 0;
+         public frmJasmine()
+         {
+             InitializeComponent();
+             errorProvider1.Clear();
+             cmbInputType.SelectedIndex = 0;
+         }
+
+       
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnJsonSelect_Click(object sender, EventArgs e)
+        {
+            JsonFileSelector.ShowDialog();
+            txtSource.Text = JsonFileSelector.FileName;
+        }
+
+        private void btnDestSelect_Click(object sender, EventArgs e)
+        {
+            DestFolderSelector.ShowDialog();
+            txtDest.Text = DestFolderSelector.SelectedPath;
+        }
+
+     
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            Int32 rowid;
+            rowid = Convert.ToInt32(e.Node.Tag);
+            dt.Rows[rowid - 1]["Active"] = e.Node.Checked;
+            foreach (TreeNode childnode in e.Node.Nodes)
+            {
+                childnode.Checked = e.Node.Checked;
+                rowid=Convert.ToInt32( childnode.Tag);
+                dt.Rows[rowid-1]["Active"] = e.Node.Checked;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            ClearGridValues();
+            bEditMode = false;
+            intParentId = Convert.ToInt32(e.Node.Tag);
+            if (e.Node.GetNodeCount(false)==0 ) //should be done only for child nodes
+            {
+                txtFieldName.Text = e.Node.Text;
+            }
+            else
+            {
+                txtFieldName.Text = string.Empty;
+            }
+            cmbJasmineExpect.Text = string.Empty;
+            txtActualValue.Text = string.Empty;
+            txtExpectedValue.Text = string.Empty;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int irow;
+            DataGridViewRow dvrow;
+            if (ValidateGridItems())
+            {
+                if (bEditMode)
+                {
+                    irow = intGridRow;
+                }
+                else
+                {
+                    irow = dataGridView1.Rows.Add();
+                }
+                
+                dvrow = dataGridView1.Rows[irow];
+                dvrow.Cells["ID"].Value = intParentId.ToString();
+                dvrow.Cells["FieldName"].Value = txtFieldName.Text;
+                dvrow.Cells["Pre"].Value = cmbPre.Text;
+                dvrow.Cells["JasmineExpect"].Value = cmbJasmineExpect.Text;
+                dvrow.Cells["ActualValue"].Value = txtActualValue.Text;
+                dvrow.Cells["ExpectedValue"].Value = txtExpectedValue.Text;
+                ClearGridValues();
+            }
+        }
+
+        private void ClearGridValues()
+        {
+            bEditMode = false;
+            txtFieldName.Text = string.Empty;
+            cmbPre.Text = string.Empty;
+            cmbJasmineExpect.Text = string.Empty;
+            txtActualValue.Text = string.Empty;
+            txtExpectedValue.Text = string.Empty;
+        }
+
+        private bool ValidateGridItems()
+        {
+            errorProvider1.Clear();
+            if (txtFieldName.Text==string.Empty)
+            {
+                errorProvider1.SetError(txtFieldName, "Field Name cannot be empty");
+                return false;
+            }
+            else if (cmbJasmineExpect.Text == string.Empty)
+            {
+                errorProvider1.SetError(cmbJasmineExpect, "Jasmine conditon cannot be empty");
+                return false;
+            }
+           
+            return true;
+            
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ClearGridValues();
+        }
+
+        private void dataGridView1_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+                bEditMode = true;
+                intGridRow = e.RowIndex;
+                DataGridViewRow dvrow = dataGridView1.Rows[intGridRow];
+                intParentId = Convert.ToInt32(dvrow.Cells["ID"].Value);
+                txtFieldName.Text = dvrow.Cells["FieldName"].Value.ToString();
+                cmbPre.Text = dvrow.Cells["Pre"].Value.ToString();
+                cmbJasmineExpect.Text = dvrow.Cells["JasmineExpect"].Value.ToString();
+                txtActualValue.Text = dvrow.Cells["ActualValue"].Value.ToString();
+                txtExpectedValue.Text = dvrow.Cells["ExpectedValue"].Value.ToString();
+            
+            
+        }
+
+        private void btnCreateJasmineTest_Click(object sender, EventArgs e)
+        {
+            if (txtDest.Text.Trim()== string.Empty )
+            {
+                errorProvider1.SetError(txtDest, "Please select the Destination folder");
+                return;
+
+            }
+            else if(txtJavascriptFile.Text.Trim() == string.Empty)
+            {
+                errorProvider1.SetError(txtJavascriptFile,"Please select the Javascript file for creating jasmine test");
+                return;
+            }
+            createJasmineTest();
+        }
+
+        private void createJasmineTest()
+        {
+            string jFileContent = string.Empty;
+            List<string> jMethods = new List<string>();
+            if(!File.Exists(txtJavascriptFile.Text))
+            {
+                MessageBox.Show("The javascript file selected does not exist");
+                return; 
+            }
+            jFileContent = File.ReadAllText(txtJavascriptFile.Text);
+            if (jFileContent.Trim().Length<=0)
+            {
+                MessageBox.Show("The javascript file is empty");
+                return;
+            }
+            jMethods = FindAllMethods(jFileContent);
+
+        }
+
+        private List<string> FindAllMethods(string jFileContent)
+        {
+            return new List<string>(); // to be modified
+        }
+
+        private void btnJavascriptFile_Click(object sender, EventArgs e)
+        {
+            JsonFileSelector.ShowDialog();
+            txtJavascriptFile.Text = JsonFileSelector.FileName;
+        }
+
+   
+
+        private void cmbInputType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbInputType.Text=="JSON")
+            {
+                pnlJSFileSelection.Visible = false;
+                btnCreateJSTree.Visible = false;
+                btnCreateJSONTree.Visible = true;
+                
+            }
+            else if(cmbInputType.Text=="Javascript")
+            {
+                pnlJSFileSelection.Visible = true;
+                btnCreateJSTree.Visible = true;
+                btnCreateJSONTree.Visible = false;
+
+            }
+        }
+
+        private void btnCreateJSTree_Click(object sender, EventArgs e)
+        {
+            string jsString = string.Empty;
+
+            if (txtJavascriptFile.Text == string.Empty)
+            {
+                errorProvider1.SetError(txtSource, "Please select a valid Javascript File");
+                return;
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+
+            jsString = txtJavascriptFile.Text;
+            Helper.JscriptReader(jsString, ref dt);
+            Helper.CreateTreeView(treeView1, dt);
+        }
+
+        private void btnCreateJSONTree_Click(object sender, EventArgs e)
+        {
+            string jsonstring = string.Empty;
+
+            if (txtSource.Text == string.Empty)
+            {
+                errorProvider1.SetError(txtSource, "Please select a valid JSON source");
+                return;
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+
+            jsonstring = File.ReadAllText(txtSource.Text);
+            Helper.JsonReader(jsonstring, ref dt);
+            Helper.CreateTreeView(treeView1, dt);
+        }
+
+
+       
+
+
+       
+    }
+}
